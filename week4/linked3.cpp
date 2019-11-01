1db7b632366bf98152b3d2eb691080a3ef12c7ca
kangsj123 <kangsj123@hanyang.ac.kr>
2019-11-02 00:32:24 +0900

    03

--- week4/linked3.cpp
+++ week4/linked3.cpp
@@ -10,14 +10,16 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         int len1=0, len2=0;
-        ListNode* tmp=l1,*tmp2=l2, *newnode=new ListNode(0);
+        ListNode* tmp;
+        tmp=l1;
         while(tmp!=NULL){
             len1++;
             tmp=tmp->next;
         }
-        while(tmp2!=NULL){
+        tmp=l2;
+        while(tmp!=NULL){
             len2++;
-            tmp2=tmp2->next;
+            tmp=tmp->next;
         }
         if(len1<len2){
             swap(l1,l2);
@@ -27,10 +29,11 @@ public:
         addFunc(l1,l2,len1,len2);
         if(l1->val>=10){
             l1->val%=10;
-            newnode->next=l1;
-            newnode->val=1;
-            return newnode;
-        }  
+            tmp=new ListNode(0);
+            tmp->next=l1;
+            tmp->val=1;
+            return tmp;
+        }
         return l1;
         
     }
@@ -38,15 +41,15 @@ public:
         if(len1==len2){
             if(first->next!=NULL && second->next!=NULL)
                 addFunc(first->next,second->next,len1-1,len2-1);
-            first->val=first->val+second->val; 
+            first->val=first->val+second->val;
         }
         else
             addFunc(first->next,second,len1-1,len2);
         
         if(first->next!=NULL && first->next->val >=10){
-                first->next->val%=10;
-                first->val++;
-        }     
+            first->next->val%=10;
+            first->val++;
+        }
     }
     
 };
