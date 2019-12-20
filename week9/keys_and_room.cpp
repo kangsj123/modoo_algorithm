cbfd8762f9813f677a4f1e6d213fcf740f6f0313
kangsj123 <kangsj123@hanyang.ac.kr>
2019-12-21 03:23:52 +0900

    tree02

---
+++ week9/keys_and_room.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        
+        int sz=rooms.size();
+
+        vector<bool>visit;
+        visit.resize(sz,false);
+        
+        queue<int>qu;
+        qu.push(0);
+        visit[0]=true;
+        
+        while(!qu.empty()){
+            int node=qu.front();
+            qu.pop();
+            
+            for(int i:rooms[node]){
+                if(!visit[i]){
+                    qu.push(i);
+                    visit[i]=true;
+                }
+            }
+        }
+        
+        for(int i=0;i<sz;i++){
+            if(!visit[i])
+                return false;
+        }
+        
+        return true;
+    }
+};
